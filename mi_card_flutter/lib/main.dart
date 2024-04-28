e13a31cedfc3e694aa876e24a1268feb7688cc01
AKD <akdusa20@gmail.com>
2024-04-28 13:51:19 -0400

    commit"

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        // Safe Area makes it so that content doesn't bleed past the bezels
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/akdas.jpg'),
+              ),
+              Text(
+                "Arnesh Das",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+
+                ),
+              ),
+            ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
+
