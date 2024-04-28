480d7a439358302df992390b56a278a8855b0c80
AKD <akdusa20@gmail.com>
2024-04-28 18:00:48 -0400

    commit

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -7,26 +7,91 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         // Safe Area makes it so that content doesn't bleed past the bezels
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/akdas.jpg'),
               ),
-              Text(
+              const Text(
                 "Arnesh Das",
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
-
                 ),
               ),
+              Text(
+                "WEB/APP DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  // Always put padding on the child
+                  padding: const EdgeInsets.all(5.0),
+                  child: ListTile(
+                    leading:
+                    const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "+1 267 992 6353",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ListTile(
+                    leading:
+                    const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title:
+                    Text(
+                      "akdusa20@gmail.com",
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           )
         ),
@@ -34,5 +99,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
