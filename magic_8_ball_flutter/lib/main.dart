c51a4fd704d4bb7a2993f7afd5324b44eebda1ec
AKD <akdusa20@gmail.com>
2024-05-27 15:49:41 -0400

    commit

--- magic_8_ball_flutter/lib/main.dart
+++ magic_8_ball_flutter/lib/main.dart
@@ -1,5 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(
+    MaterialApp (
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text("Ask Me Anything"),
+          backgroundColor: Colors.blueAccent.shade400,
+        ),
+        body: BallPage(),
+      ),
+    )
+  );
 }
+
+class BallPage extends StatefulWidget {
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int number = 1;
+
+  void changeNumber() {
+    number = Random().nextInt(4) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  changeNumber();
+                });
+              },
+                child: Image.asset('images/ball$number.png')
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
